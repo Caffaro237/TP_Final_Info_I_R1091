aacf2b5cc03423e2baec3a6cd615f4b24877e897
Benjamin Rancivilla <brancivilla@frba.utn.edu.ar>
2025-11-27 16:18:16 -0300

    Ahi lo arregle

--- Alta_de_remito.c
+++ Alta_de_remito.c
@@ -13,9 +13,9 @@ int mostrar_por_id (int id, NODO_CLI* top_cliente, NODO_EQUI* top_equipo);
 void crear_equipo (NODO_EQUI** top_equipo, EQUIPO datos)
 {
   NODO_EQUI* equipo_nuevo=(NODO_EQUI*) malloc(sizeof(NODO_EQUI)); //Creo el equipo
-  NODO_EQUI* aux = *top_equipo;
   equipo_nuevo -> data=datos;
   equipo_nuevo -> next=NULL;
+  NODO_EQUI* aux = *top_equipo;
     
   if (*top_equipo!=NULL) // Si la lista ya existe en forma de memoria dinamica
   { 
@@ -28,29 +28,19 @@ void crear_equipo (NODO_EQUI** top_equipo, EQUIPO datos)
     aux->next=equipo_nuevo; //El ultimo elemento apunta al nuevo equipo
   }
   
-  else //No existe la lista, la tengo que crear
+  else //No existe la lista, hago que el primer elemento de la lista sea el ingresado
   {
-    //*top_equipo=FUNCION FACHA DE FACU PARA LEER ARCHIVOS;
-    *top_equipo = -1; //BORRAR ESTO, PONER LO DE ARRIBA
-    if (*top_equipo==-1) //Si falla la funcion de facu, creo una lista que su primer elemento es el creado
-    {
-      *top_equipo=equipo_nuevo;
-      equipo_nuevo -> data.numero_de_orden = 0;
-      printf ("OJITO, no encontre la lista de equipos en el archivo, me hice una nueva\n");
-    }
-    else
-    {
-      crear_equipo (top_equipo, datos); //Llamo de vuelta a la función, pero ahora top si apunta a una lista
-    }
+    *top_equipo=equipo_nuevo;
   }
 }
 
 void crear_cliente (NODO_CLI** top_cliente, CLIENTE datos)
 {
   NODO_CLI* cliente_nuevo=(NODO_CLI*) malloc(sizeof(NODO_CLI)); //Creo el cliente
-  NODO_CLI* aux=*top_cliente;
   cliente_nuevo -> data=datos;
   cliente_nuevo -> next=NULL;
+  NODO_CLI* aux=*top_cliente;
+
     
   if (*top_cliente!=NULL) // Si la lista ya existe en forma de memoria dinamica
   { 
@@ -66,18 +56,7 @@ void crear_cliente (NODO_CLI** top_cliente, CLIENTE datos)
   
   else //No existe la lista, la tengo que crear
   {
-    //*top_cliente=FUNCION FACHA DE FACU PARA LEER ARCHIVOS;
-    *top_cliente=-1; //BORRAR ESTO, PONER LO DE ARRIBA
-    if (*top_cliente == -1) //Si falla la funcion de facu, creo una lista que su primer elemento es el creado
-    {
-      cliente_nuevo -> data.numero_de_orden = 0;
-      *top_cliente=cliente_nuevo;
-      printf ("OJITO, no encontre la lista de clientes en el archivo, me hice una nueva\n");
-    }
-    else
-    {
-      crear_cliente (top_cliente, datos); //Llamo de vuelta a la función, pero ahora top si apunta a una lista
-    }
+    *top_cliente=cliente_nuevo;
   }
 }
 
