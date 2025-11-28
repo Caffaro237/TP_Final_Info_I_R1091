b9a0ee6a7e4010d9965c4a0881755ba3ce8ea252
Caffaro237 <facundodarian3@gmail.com>
2025-11-28 20:22:55 -0300

    Se pasa main a carpeta Cliente

---
+++ Cliente/main.c
@@ -0,0 +1,44 @@
+#include "Headers.h"
+#include "GestorArchivos.c"
+#include "menu.c"
+
+int main (void)
+{
+    char path[200] = "./Clientes.csv";
+    char seguir = 's';
+    int c;
+
+    while(seguir == 's')
+    {
+        switch(menu()) //Llama a la funcion para mostrar la sopciones principales
+        {
+            case 1:
+                readFile(path);
+                break;
+
+            case 6:
+                seguir = 'n';
+                break;
+            
+            default:
+                printf("Opcion invalida\n");
+                break;
+        }
+
+        if(seguir == 's')
+        {
+            // Limpia cualquier caracter previo (incluye el \n que deja scanf)
+            while ((c = getchar()) != '\n' && c != EOF);
+
+            printf("Presiona ENTER para continuar...");
+            fflush(stdout);
+            getchar();
+        }
+    }
+    
+    
+    printf("Saliendo de la Aplicacion\n");
+    printf("\n");
+
+    return 0;
+}
