0aa2e5d71ffb674b63ce3f39bfd807cfce1caf98
Benjamin Rancivilla <brancivilla@frba.utn.edu.ar>
2025-12-05 11:06:22 -0300

    Headesr del amin para que ande

---
+++ Cliente/Headers.h
@@ -0,0 +1,18 @@
+#ifndef HEADERS_H
+#define HEADERS_H
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h> //Memoria dinámica
+#include <fcntl.h> //Archivos
+#include <unistd.h> //Archivos
+#include <sys/types.h> //Archivos
+#include <sys/stat.h> //Archivos//
+#include <stdint.h>
+#include "sock-lib.h"
+
+
+#include "Mostrar_datos.h"
+#include "menu.h"
+
+#endif
