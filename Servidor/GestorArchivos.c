00b41fb209021b60ff8f04a7f6d820ed8961f116
Caffaro237 <facundodarian3@gmail.com>
2025-12-07 16:00:58 -0300

    Add nuevo metodo para separar la  linea por coma y crear una estructura para cargarla a la lista enlazada

--- Servidor/GestorArchivos.c
+++ Servidor/GestorArchivos.c
@@ -1,24 +1,48 @@
+#ifndef GESTOR_ARCHIVOS_H
+#define GESTOR_ARCHIVOS_H
+
 #include "Headers.h"
 
-int readFile(char path[200])
+#define ARCHIVO_CLIENTES "./Clientes.csv"
+#define ARCHIVO_EQUIPOS "./Equipos.csv"
+#define ARCHIVO_REPARACIONES "./Reparaciones.csv"
+
+int LeerArchivo(NodoCliente *TOP_Clientes, NodoEquipo *TOP_Equipo, NodoReparaciones *TOP_Reparaciones, int tipoDato)
 {
     char *linea;
     int fdFile = 0;
 
-    fdFile = open(path, O_RDONLY);
+    char archivo[100];
+
+    switch (tipoDato)
+    {
+        case 1:
+            strcpy(archivo, ARCHIVO_CLIENTES);
+            break;
+        case 2:
+            strcpy(archivo, ARCHIVO_EQUIPOS);
+            break;
+        case 3:
+            strcpy(archivo, ARCHIVO_REPARACIONES);
+            break;
+        
+        default:
+            return -1;
+            break;
+    }
+
+    fdFile = open(archivo, O_RDONLY);
 
     if (fdFile == -1)
     {
         printf("Error al abrir archivo\n");
-        exit(-1);
-    }
-    else
-    {
-        printf("Archivo abierto correctamente\n\n");
+        return -1;
     }
 
-    while(readline(fdFile, &linea))
+    while(LeerLinea(fdFile, &linea))
     {
+        CargarDato(linea, tipoDato);
+
         printf("Linea: %s\n", linea);
 
         free(linea);
@@ -27,7 +51,7 @@ int readFile(char path[200])
     return 0;
 }
 
-int readline(int fd, char **linea)
+int LeerLinea(int fd, char **linea)
 {
     char lineaAux[2000] = "";
     char buffer = ' ';
@@ -58,7 +82,113 @@ int readline(int fd, char **linea)
     return bytesLeidos;
 }
 
-int writeFile(char path[200])
+int EscribirArchivo(CLIENTE *cliente, EQUIPO *equipo, REPARACIONES *reparaciones, int tipoDato)
 {
+    char archivo[100];
+    
+    switch (tipoDato)
+    {
+        case 1:
+            strcpy(archivo, ARCHIVO_CLIENTES);
+            break;
+        case 2:
+            strcpy(archivo, ARCHIVO_EQUIPOS);
+            break;
+        case 3:
+            strcpy(archivo, ARCHIVO_REPARACIONES);
+            break;
+        
+        default:
+            return -1;
+            break;
+    }
+
     return 0;
 }
+
+int CargarDato(char *linea, int tipoDato)
+{
+    char campos[6][50];
+
+    CLIENTE cliente;
+    EQUIPO equipo;
+    REPARACIONES reparacion;
+
+    int cantidad = SepararPorComa(linea, campos);
+
+    if(cantidad == 0)
+    {
+        return -2;
+    }   
+
+    switch (tipoDato)
+    {
+        case 1: //Carga Clientes
+
+            cliente.numero_de_orden = atoi(campos[0]);
+            strcpy(cliente.fechaIngreso, campos[1]);
+            strcpy(cliente.nombre, campos[2]);
+            strcpy(cliente.apellido, campos[3]);
+            strcpy(cliente.direccion, campos[4]);
+            strcpy(cliente.telefono, campos[5]);
+
+            //funcion cargar datos benja
+            break;
+
+        case 2: //Carga Equipos
+
+            equipo.numero_de_orden = atoi(campos[0]);
+            strcpy(equipo.tipo, campos[1]);
+            strcpy(equipo.marca, campos[2]);
+            strcpy(equipo.modelo, campos[3]);
+            strcpy(equipo.falla, campos[4]);
+
+            //funcion cargar datos benja
+            break;
+
+        case 3: //Carga Reparaciones
+
+            reparacion.numero_de_orden = atoi(campos[0]);
+            strcpy(reparacion.reparacionAEfectuar, campos[1]);
+            strcpy(reparacion.presupuesto, campos[2]);
+            strcpy(reparacion.confirmacion, campos[3]);
+            strcpy(reparacion.reparacionAEfectuar, campos[4]);
+            strcpy(reparacion.fechaEgreso, campos[5]);
+
+            //funcion cargar datos benja
+            break;
+    
+        default:
+            return -2;
+            break;
+    }
+    
+    return 0;
+}
+
+int SepararPorComa(char *linea, char campos[][50])
+{
+    int i;
+    int j = 0;
+    int k = 0;
+
+    for (i = 0; linea[i] != '\0'; i++)
+    {
+        if (linea[i] == ',')
+        {
+            campos[k][j] = '\0';
+            k++;
+            j = 0;
+        }
+        else
+        {
+            campos[k][j++] = linea[i];
+        }
+    }
+
+    campos[k][j] = '\0';
+
+    return k + 1; //Cantidad de campos encontrados
+}
+
+#endif
