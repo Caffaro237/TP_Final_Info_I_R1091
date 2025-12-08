80e9bd1da4d4f54795466c53eac6c459278e4cb0
Caffaro237 <facundodarian3@gmail.com>
2025-12-08 16:26:18 -0300

    Rework prototipos para incluir punteros a punteros

--- Servidor/Prototipos.h
+++ Servidor/Prototipos.h
@@ -1,13 +1,13 @@
 //Prototipos de Funciones
 
 int menu(void);
-int inicializar(NodoCliente *TOP_Clientes, NodoEquipo *TOP_Equipo, NodoReparaciones *TOP_Reparaciones);
+int inicializar(NodoCliente **TOP_Clientes, NodoEquipo **TOP_Equipo, NodoReparaciones **TOP_Reparaciones);
 
 //Funciones para manejo archivos
-int LeerArchivo(NodoCliente *TOP_Clientes, NodoEquipo *TOP_Equipo, NodoReparaciones *TOP_Reparaciones, int tipoDato);
+int LeerArchivo(NodoCliente **TOP_Clientes, NodoEquipo **TOP_Equipo, NodoReparaciones **TOP_Reparaciones, int tipoDato);
 int LeerLinea(int fd, char **linea);
 int EscribirArchivo(CLIENTE *cliente, EQUIPO *equipo, REPARACIONES *reparaciones, int tipoDato);
-int CargarDato(char *linea, int tipoDato);
+int CargarDato(NodoCliente **TOP_Clientes, NodoEquipo **TOP_Equipo, NodoReparaciones **TOP_Reparaciones, char *linea, int tipoDato);
 int SepararPorComa(char *linea, char campos[][50]);
 
 //Funciones para altas de clientes y equipos
