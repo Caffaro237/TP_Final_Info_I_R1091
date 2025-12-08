0eb05eb8a28f2322bfc6b7fc7e0fe7bc63d7d666
Caffaro237 <facundodarian3@gmail.com>
2025-12-08 18:12:29 -0300

    Fix errores en main servidor y cliente

--- Cliente/main.c
+++ Cliente/main.c
@@ -2,77 +2,77 @@
 
 int main (void)
 {
-  char datos[300];
-  int32_t opcion=0;
-  int32_t num_de_orden=0;
-  int existe_el_cliente=0;
+    char datos[300];
+    int32_t opcion=0;
+    int32_t num_de_orden=0;
+    int existe_el_cliente=0;
+    int32_t fin_de_la_lista;
 
+    int sock;
+    sock = conectar("localhost", 8000, 1);
 
-  int sock;
-  sock = conectar("localhost", 8000, 1);
-
-  while(opcion != 6)
-  {
-    opcion = menu();
-    write(sock, &opcion, sizeof(int32_t));
-    
-    switch(opcion) 
+    while(opcion != 6)
     {
+        fin_de_la_lista=0;
+        strcpy(datos,"");
 
-      case 2:
-        printf("Escriba los datos del cliente: ");
-        scanf("%299s", datos);
-        write(sock, datos, 300);
-        printf("Escriba los datos del equipo: ");
-        scanf("%299s", datos);
-        write(sock, datos, 300);
-        pausa();
-        break;
-
-      case 5:
-        printf("Escriba el numero de orden: ");
-        scanf("%d", &num_de_orden);
-        //Envio el numero de orden
-        write(sock, &num_de_orden, sizeof(int32_t));
-        //Muestro el cliente
-        read(sock, &existe_el_cliente, sizeof(int));
-        if (existe_el_cliente)
+        opcion = menu();
+        write(sock, &opcion, sizeof(int32_t));
+      
+        switch(opcion) 
         {
-          read(sock, datos, 300);
-          printf("Cliente:\n");
-          Mostrar_cadena(datos);
-          strcpy(datos, "");
-          //Muestro el equipo
-          read(sock, datos, 300);
-          printf("\nEquipo:\n");
-          Mostrar_cadena(datos);
 
+          case 2:
+                printf("Escriba los datos del cliente: ");
+                scanf("%299s", datos);
+                write(sock, datos, 300);
+                printf("Escriba los datos del equipo: ");
+                scanf("%299s", datos);
+                write(sock, datos, 300);
+                pausa();
+                break;
+
+          case 5:
+                printf("Escriba el numero de orden: ");
+                scanf("%d", &num_de_orden);
+                
+                //Envio el numero de orden
+                write(sock, &num_de_orden, sizeof(int32_t));
+                
+                //Muestro el cliente
+                read(sock, &existe_el_cliente, sizeof(int));
+                
+                if(existe_el_cliente)
+                {
+                    read(sock, datos, 300);
+                    printf("Cliente:\n");
+                    Mostrar_cadena(datos);
+                    strcpy(datos, "");
+                    //Muestro el equipo
+                    read(sock, datos, 300);
+                    printf("\nEquipo:\n");
+                    Mostrar_cadena(datos);
+                }
+                else 
+                {
+                    printf ("No se encontro el numero de orden\n");
+                }
+
+                pausa();
+                break;
+
+            case 7:
+                printf("Saliendo de la Aplicacion\n");
+                break;
+
+            default:
+                printf("Opción invalida");
+                pausa();
+                break;
         }
-        else 
-        {
-          printf ("No se encontro el numero de orden\n");
-        }
-        pausa();
-        break;
-
-      case 7:
-        printf("Saliendo de la Aplicacion\n");
-        break;
-
-      default:
-        printf("Opción invalida");
-        pausa();
-        break;
-    
-    
     }
-
-
-  }
   
-  
-  
-  printf("\n");
+    printf("\n");
 
-  return 0;
+    return 0;
 }
