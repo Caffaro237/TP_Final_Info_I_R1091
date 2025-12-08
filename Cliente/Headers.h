68d4e4404b4e8d1c078953b810ca1ce7633a235b
Caffaro237 <facundodarian3@gmail.com>
2025-12-08 18:48:43 -0300

    Add include para sockets

--- Cliente/Headers.h
+++ Cliente/Headers.h
@@ -1,6 +1,3 @@
-#ifndef HEADERS_H
-#define HEADERS_H
-
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h> //Memoria dinámica
@@ -9,7 +6,16 @@
 #include <sys/types.h> //Archivos
 #include <sys/stat.h> //Archivos//
 #include <stdint.h>
+#include <errno.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <sys/wait.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+
+
+#define PORT 7726  /* El puerto donde se conectará, servidor */
+#define BACKLOG 2 /* Tamaño de la cola de conexiones recibidas */
+
 
 #include "Prototipos.h"
-
-#endif
