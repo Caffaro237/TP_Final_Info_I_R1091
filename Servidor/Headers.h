b790e838efb42fa50806ee4a4eb123f51c2218ef
brancivilla <brancivilla@frba.utn.edu.ar>
2025-12-11 23:49:59 -0300

    Agregue constantes que son el maximo de cada variable de cada estructura. Por si en algun momento lo queremos cambiar, es mas sencillo. Tambien me sirvio para ciertas partes dle código

--- Servidor/Headers.h
+++ Servidor/Headers.h
@@ -17,6 +17,23 @@
 #define PORT 7726  /* El puerto donde se conectará, servidor */
 #define BACKLOG 2 /* Tamaño de la cola de conexiones recibidas */
 
+#define MAX_FECHA_INGRESO 11
+#define MAX_NOMBRE 15
+#define MAX_APELLIDO 15
+#define MAX_DIRECCION 20
+#define MAX_TELEFONO 10
+
+#define MAX_TIPO 15
+#define MAX_MODELO 15
+#define MAX_MARCA 20
+#define MAX_FALLA 20
+
+#define MAX_REPARACION 15
+#define MAX_PRESUPUESTO 15
+#define MAX_CONFIRMACION 20
+#define MAX_REPARADO 20
+#define MAX_FECHA_EGRESO 11
+
 
 #include "Estructuras.h"
 #include "Prototipos.h"
