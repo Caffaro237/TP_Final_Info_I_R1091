ebbb690c75b2999fdc8fc3761bb8bab82a68a3f7
Caffaro237 <facundodarian3@gmail.com>
2025-12-14 13:45:51 -0300

    Modificacion logueos de conexion por sockets

--- Cliente/socket.c
+++ Cliente/socket.c
@@ -5,6 +5,7 @@ int conectar(char *hostname, int port, int debug)
 	int sockfd;
 	struct hostent *he;			   /* Se utiliza para convertir el nombre del host a su dirección IP */
 	struct sockaddr_in their_addr; /* dirección del server donde se conectará */
+	char ipstr[INET_ADDRSTRLEN];
 
 	// convertimos el nombre del host a su dirección IP
 	if ((he = gethostbyname(hostname)) == NULL)
@@ -19,11 +20,6 @@ int conectar(char *hostname, int port, int debug)
 		perror("Error en creación de socket");
 		exit(1);
 	}
-	if (debug)
-		fprintf(stderr, "debug:: conectar() socket()=%d\t\t..........OK\n", sockfd);
-
-	if (debug)
-		fprintf(stderr, "debug:: conectar() dst port()=%d\t..........OK\n", port);
 
 	port = (port == 0) ? htons(PORT) : htons(port);
 
@@ -39,8 +35,16 @@ int conectar(char *hostname, int port, int debug)
 		perror("conectar():: error tratando de conectar al server");
 		exit(1);
 	}
+
+	inet_ntop(AF_INET, &their_addr.sin_addr, ipstr, sizeof ipstr);
+
 	if (debug)
-		fprintf(stderr, "debug:: conectar() connect()\t\t..........OK\n");
+	{
+		printf(stderr, "Conectando a %s\n", hostname);
+		printf(stderr, "IP %s:%d\t\t..........OK\n", ipstr, ntohs(their_addr.sin_port));
+	}
+	
+	
 
 	return sockfd;
 }
