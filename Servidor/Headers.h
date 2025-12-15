4c05fbc040fbae27c95f65eace122efa6b90da66
Caffaro237 <facundodarian3@gmail.com>
2025-12-14 23:06:22 -0300

    Add mas espacio para campos

--- Servidor/Headers.h
+++ Servidor/Headers.h
@@ -17,22 +17,22 @@
 #define PORT 7726  /* El puerto donde se conectará, servidor */
 #define BACKLOG 2 /* Tamaño de la cola de conexiones recibidas */
 
-#define MAX_FECHA_INGRESO 11
-#define MAX_NOMBRE 15
-#define MAX_APELLIDO 15
+#define MAX_FECHA_INGRESO 50
+#define MAX_NOMBRE 50
+#define MAX_APELLIDO 50
 #define MAX_DIRECCION 50
-#define MAX_TELEFONO 11
+#define MAX_TELEFONO 50
 
-#define MAX_TIPO 15
-#define MAX_MODELO 15
-#define MAX_MARCA 20
-#define MAX_FALLA 100
+#define MAX_TIPO 50
+#define MAX_MODELO 50
+#define MAX_MARCA 50
+#define MAX_FALLA 200
 
-#define MAX_REPARACION 100
+#define MAX_REPARACION 200
 #define MAX_PRESUPUESTO 15
 #define MAX_CONFIRMACION 5
 #define MAX_REPARADO 5
-#define MAX_FECHA_EGRESO 11
+#define MAX_FECHA_EGRESO 50
 
 
 #include "Estructuras.h"
