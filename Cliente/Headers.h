18e7605f134fccaf2b63ca73ea3b26b1c3685a60
brancivilla-ctrl <brancivilla@frba.utn.edu.ar>
2026-01-20 16:17:46 -0300

    Update Headers.h
    Agregue las constantes utilizadas para el tamaño de las cadenas de caracteres y las opciones del menu

--- Cliente/Headers.h
+++ Cliente/Headers.h
@@ -18,22 +18,38 @@
 #define SERVER "reparacionespcmaster.ddns.net"  /* Servidor al que se conectará*/
 #define PORT 7726  /* El puerto donde se conectará, servidor */
 
+//Longitud para clase Cliente
 #define MAX_FECHA_INGRESO 50
 #define MAX_NOMBRE 50
 #define MAX_APELLIDO 50
 #define MAX_DIRECCION 50
 #define MAX_TELEFONO 50
 
+//Longitud para clase Equipo
 #define MAX_TIPO 50
 #define MAX_MODELO 50
 #define MAX_MARCA 50
 #define MAX_FALLA 200
 
+//Longitud para clase reparaciones
 #define MAX_REPARACION 200
 #define MAX_PRESUPUESTO 15
 #define MAX_CONFIRMACION 5
 #define MAX_REPARADO 5
 #define MAX_FECHA_EGRESO 50
 
+//Opciones del menu
+#define LISTAR_CLIENTES 1
+#define ALTA_DE_CLIENTE 2
+#define GENERAR_REPARACION 3
+#define MODIFICAR_DATOS_CLIENTE 4
+#define MODIFICAR_DATOS_EQUIPO 5
+#define BUSCAR_CLIENTE 6
+#define ENVIAR_WHATSAPP 7
+#define SALIR 8
+
+//Tamaños de variables para manejo de datos
+#define MAX_DATOS 300
+#define MAX_DATOS_BUFFER 1000
 
 #include "Prototipos.h"
